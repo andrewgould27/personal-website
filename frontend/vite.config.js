08a4b8272ff7e2519baad98104604f9403eb618c
andrewgould27 <andrewgould935@gmail.com>
2026-06-06 14:48:25 -0400

    hotfix: resolve docker issues and vite conf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    allowedHosts: ['andrewgould.dev', 'www.andrewgould.dev'],
     proxy: {
       '/api': process.env.API_URL ?? 'http://localhost:8000',
     },
